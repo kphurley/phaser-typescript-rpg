515c205922a5840c97bd7b9c5c5365b99cef5b9b
Kevin Hurley <kphurley@gmail.com>
2019-02-01 21:02:52 -0600

    Add linter support with gts, and fix all current files

--- src/scenes/TestScene.ts
+++ src/scenes/TestScene.ts
@@ -1,28 +1,19 @@
-import RegularHexagonTesselation from '../util/RegularHexagonTesselation';
+import {RegularHexagonTesselation} from '../util/RegularHexagonTesselation';
 
-class TestScene extends Phaser.Scene {
-	polygons: Array<Phaser.GameObjects.Polygon>;
-	cursors: any;
+export class TestScene extends Phaser.Scene {
+  polygons!: Phaser.GameObjects.Polygon[];
 
-	constructor() {
-    super({
-			key: 'TestScene'
-		});
-	}
-	
-	preload() {
+  constructor() {
+    super({key: 'TestScene'});
+  }
 
-	}
+  preload() {}
 
-	create() {
-		const tesselation = new RegularHexagonTesselation(4);
-		const regularHexagons = tesselation.createPolygons(200, 200, 60, 0xffffff);
-		this.polygons = tesselation.addToScene(this, regularHexagons);
+  create() {
+    const tesselation = new RegularHexagonTesselation(4);
+    const regularHexagons = tesselation.createPolygons(200, 200, 60, 0xffffff);
+    this.polygons = tesselation.addToScene(this, regularHexagons);
+  }
 
-	}
-
-	update(time: number, delta:number) {
-	}
+  update(time: number, delta: number) {}
 }
-
-export default TestScene;
