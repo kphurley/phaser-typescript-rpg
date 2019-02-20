b31d308f1ef9123b99c7029a3c90c2ab34a047d7
Kevin Hurley <kphurley@gmail.com>
2019-02-19 22:11:34 -0600

    add basic animations to sprite entities

--- src/entities/SpriteEntity.ts
+++ src/entities/SpriteEntity.ts
@@ -17,9 +17,42 @@ export class SpriteEntity extends GridEntity {
     this.sprite = scene.add.sprite(x, y, spriteRef);
     scene.hexagonGrid.assignEntityToGridLocation(this, location);
     this.sprite.setData('blocker', true);  // TODO - DO WE NEED THIS?
+
+    this.configureAnimations();
   }
 
   isEmpty(): boolean {
     return false;
+  }
+
+  configureAnimations() {
+    const idleConfig = {
+      key: `${this.name}-idle`,
+      frames: this.scene.anims.generateFrameNumbers(
+          this.spriteRef, {start: 0, end: 1, first: 0}),
+      frameRate: 6,
+      repeat: -1
+    };
+
+    // TODO - get from config
+    const walkConfig = {
+      key: `${this.name}-walk`,
+      frames: this.scene.anims.generateFrameNumbers(
+          'base_walk', {start: 0, end: 3, first: 0}),
+      frameRate: 6,
+      repeat: -1
+    };
+
+    this.scene.anims.create(idleConfig);
+    this.scene.anims.create(walkConfig);
+    this.sprite.anims.play(`${this.name}-idle`);
+  }
+
+  setIdle() {
+    this.sprite.anims.play(`${this.name}-idle`);
+  }
+
+  setWalking() {
+    this.sprite.anims.play(`${this.name}-walk`);
   }
 }
--- src/scenes/GridScene.ts
+++ src/scenes/GridScene.ts
@@ -1,3 +1,4 @@
+import {MonsterEntity} from '../entities/monster/MonsterEntity';
 import {PlayerEntity} from '../entities/player/PlayerEntity';
 import {GridSceneStateManager} from '../states/GridSceneStateManager';
 import {HexagonGrid} from '../util/HexagonGrid';
@@ -18,7 +19,7 @@ export class GridScene extends Phaser.Scene {
 
   preload() {
     // TODO - Extract to config or preload helper
-    this.load.image('test_kenney', 'assets/sprites/dirt_08_60x70.png');
+    this.load.image('test_kenney', 'assets/sprites/grass_05_60x70.png');
     this.load.image('slime', 'assets/sprites/slime_64.png');
     this.load.image('ranger', 'assets/sprites/ranger.png');
     this.load.image('warrior', 'assets/sprites/warrior.png');
@@ -26,6 +27,17 @@ export class GridScene extends Phaser.Scene {
     this.load.image('quickMove', 'assets/sprites/icons/fire-dash.png');
     this.load.image('move', 'assets/sprites/icons/run.png');
     this.load.image('confirm', 'assets/sprites/icons/play-button.png');
+
+    this.load.spritesheet(
+        'base_idle', 'assets/sprites/base_idle.png',
+        {frameWidth: 64, frameHeight: 64, endFrame: 1});
+    this.load.spritesheet(
+        'base_walk', 'assets/sprites/base_walk.png',
+        {frameWidth: 64, frameHeight: 64, endFrame: 2});
+
+    this.load.spritesheet(
+        'slime_idle', 'assets/sprites/slime_idle.png',
+        {frameWidth: 64, frameHeight: 64, endFrame: 1});
   }
 
   create() {
@@ -34,12 +46,17 @@ export class GridScene extends Phaser.Scene {
 
     // TODO - Is this the best place to create the entities?
     // TODO - Extract the 'skills' config someplace else, maybe its own class?
-    const playerWarrior = new PlayerEntity(
-        this, 'playerWarrior', 'warrior', `3,2`, {skills: ['quickMove']});
-    const playerWizard = new PlayerEntity(
-        this, 'playerWizard', 'wizzard', `4,2`, {skills: ['move']});
-    const playerRanger = new PlayerEntity(
-        this, 'playerRanger', 'ranger', `5,2`, {skills: ['quickMove']});
+    // const playerWarrior = new PlayerEntity(
+    //     this, 'playerWarrior', 'warrior', `3,2`, {skills: ['quickMove']});
+    // const playerWizard = new PlayerEntity(
+    //     this, 'playerWizard', 'wizzard', `4,2`, {skills: ['move']});
+    // const playerRanger = new PlayerEntity(
+    //     this, 'playerRanger', 'ranger', `5,2`, {skills: ['quickMove']});
+
+    const player = new PlayerEntity(
+        this, 'player', 'base_idle', `3,2`, {skills: ['quickMove']});
+
+    const slime = new MonsterEntity(this, 'slime', 'slime_idle', `7,2`);
 
     // TODO:  Place enemies on the grid
 
